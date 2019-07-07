e1cda28a4d505f3ed46bb580da0af70acc3bf1b7
danielbh <danielhollcraft@gmail.com>
2019-07-07 10:12:55 -0400

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/danielbh/s3-upload-script-go
+
+go 1.12
+
+require github.com/aws/aws-sdk-go v1.20.15 // indirect
---
+++ main.go
@@ -0,0 +1,50 @@
+/*
+https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/go/example_code/s3/s3_upload_object.go
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("usage: %s <bucket> <filename>\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	bucket := os.Args[1]
+	filename := os.Args[2]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Failed to open file", filename, err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	//select Region to use.
+	conf := aws.Config{Region: aws.String("us-east-1")}
+	sess := session.New(&conf)
+	svc := s3manager.NewUploader(sess)
+
+	fmt.Println("Uploading file to S3...")
+	result, err := svc.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filepath.Base(filename)),
+		Body:   file,
+	})
+	if err != nil {
+		fmt.Println("error", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully uploaded %s to %s\n", filename, result.Location)
+}
